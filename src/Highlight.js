f34fc7c8ca0fe4551eefe76a5cc3478c39a33824
death667b <R0440ver$>
2017-04-05 22:32:24 +1000

    update highligh react component with hightlight.js import

--- src/Highlight.js
+++ src/Highlight.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { findDOMNode } from 'react-dom'
-import highlight from 'highlight.js'
+import highlight from 'mc-highlight.js'
 
 export default class Highlight extends Component {
   static propTypes = {
